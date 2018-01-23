4ce13e007a27c85eadad6b07e0d103d7847a5bbf
WS <aa1010111@icloud.com>
2018-01-23 09:35:34 +0800

    add container

--- test.js
+++ test.js
@@ -6,3 +6,7 @@ function putFromNameIn() {
     var x = document.getElementById("inputFromName").value;
     document.getElementById("fromName").innerText = x;
 }
+function goodLuckHand() {
+    document.getElementById("getName").value = "幹案三小ㄛ";
+    document.getElementById("inputFromName").value = "自己想很難??";
+}
--- test.ts
+++ test.ts
@@ -6,3 +6,7 @@ function putFromNameIn() {
     const x = (document.getElementById("inputFromName") as HTMLInputElement).value;
     document.getElementById("fromName").innerText = x;
 }
+function goodLuckHand() {
+    (document.getElementById("getName") as HTMLInputElement).value = "幹案三小ㄛ";
+    (document.getElementById("inputFromName") as HTMLInputElement).value = "自己想很難??";
+}

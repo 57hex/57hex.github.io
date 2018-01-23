1d3db3a760559cf4ae4958a3dba878c71a491c5e
WS <aa1010111@icloud.com>
2018-01-23 21:32:05 +0800

    add food

---
+++ food/main.js
@@ -0,0 +1,11 @@
+function generateRandomInteger(min, max) {
+    return Math.floor(min + Math.random() * (max + 1 - min));
+}
+function beginRandomInt() {
+    document.getElementById("inputNum").value = generateRandomInteger(0, 50).toString();
+}
+function calc() {
+    var x = document.getElementById("inputNum").value;
+    document.getElementById("total").textContent = (x * 50).toString();
+}
+beginRandomInt();
---
+++ food/main.ts
@@ -0,0 +1,11 @@
+function generateRandomInteger(min, max) {
+    return Math.floor(min + Math.random() * (max + 1 - min))
+}
+function beginRandomInt(){
+    (document.getElementById("inputNum") as HTMLInputElement).value = generateRandomInteger(0, 50).toString();
+}
+function calc(){
+    var x = (document.getElementById("inputNum") as HTMLInputElement).value;
+    document.getElementById("total").textContent = (x * 50).toString();
+}
+beginRandomInt();

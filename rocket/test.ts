cefd54f00401b5921301abae9afe714ca5a5fe86
WS <aa1010111@icloud.com>
2018-01-31 17:57:15 +0800

    rocket update.

--- rocket/test.ts
+++ rocket/test.ts
@@ -2,30 +2,49 @@ function generateRandomInteger(min, max) {
     return Math.floor(min + Math.random() * (max + 1 - min));
 }
 let body = document.body;
-const a = (document.querySelector(".rocket-1") as HTMLElement).style.bottom;
-const b = (document.querySelector(".rocket-2") as HTMLElement).style.bottom;
-const c = (document.querySelector(".rocket-3") as HTMLElement).style.bottom;
+const rocket1 = (document.querySelector(".rocket-1") as HTMLElement);
+const rocket2 = (document.querySelector(".rocket-2") as HTMLElement);
+const rocket3 = (document.querySelector(".rocket-3") as HTMLElement);
+// 預先保存原本的位置，方便火箭飛回來回來
+const rocketLocarion = [
+    {
+        a: rocket1.style.bottom,
+        b: rocket1.style.left,
+    },
+    {
+        a: rocket2.style.bottom,
+        b: rocket2.style.left,
+    },
+    {
+        a: rocket3.style.bottom,
+        b: rocket3.style.left,
+    },
+];
 body.addEventListener("keydown", goRocket , false);
 function goRocket(e) {
-    console.log(e.keyCode);
-
     switch (e.keyCode) {
         case 49:
-            (document.querySelector(".rocket-1") as HTMLElement).style.bottom = "2000px";
+            rocket1.style.bottom = "1000px";
+            rocket1.style.left = "1000px";
             break;
         case 50:
-            (document.querySelector(".rocket-2") as HTMLElement).style.bottom = "2000px";
+            rocket2.style.bottom = "1000px";
+            rocket2.style.left = "1000px";
             break;
         case 51:
-            (document.querySelector(".rocket-3") as HTMLElement).style.bottom = "2000px";
+            rocket3.style.bottom = "1000px";
+            rocket3.style.left = "1000px";
             break;
         case 81:
-            (document.querySelector(".rocket-1") as HTMLElement).style.bottom = a;
+            rocket1.style.bottom = rocketLocarion[0].a;
+            rocket1.style.left = rocketLocarion[0].b;
             break;
         case 87:
-            (document.querySelector(".rocket-2") as HTMLElement).style.bottom = b;
+            rocket2.style.bottom = rocketLocarion[1].a;
+            rocket2.style.left = rocketLocarion[1].b;
             break;
         case 69:
-            (document.querySelector(".rocket-3") as HTMLElement).style.bottom = c;
+            rocket3.style.bottom = rocketLocarion[2].a;
+            rocket3.style.left = rocketLocarion[2].b;
     }
 }

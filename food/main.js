7a14bfbd545d0555507be4502b61bd3c2e57d54b
WS <aa1010111@icloud.com>
2018-02-02 20:37:26 +0800

    add blur checker on food calc

--- food/main.js
+++ food/main.js
@@ -5,9 +5,24 @@ function beginRandomInt() {
     document.getElementById("inputCount").value = generateRandomInteger(0, 50).toString();
     document.getElementById("inputPrice").value = generateRandomInteger(50, 200).toString();
 }
+var count = document.querySelector("#inputCount");
+var price = document.querySelector("#inputPrice");
+var btn = document.querySelector("#btn");
+count.addEventListener("blur", checkNull, false);
+price.addEventListener("blur", checkNull, false);
+btn.addEventListener("click", calc, false);
+function checkNull() {
+    if (this.value === "") {
+        alert("此欄位不得為空");
+        btn.setAttribute("disabled", "true");
+    }
+    else {
+        btn.removeAttribute("disabled");
+    }
+}
 function calc() {
-    var x = parseInt(document.getElementById("inputCount").value, 0);
-    var y = parseInt(document.getElementById("inputPrice").value, 0);
+    var x = parseInt(count.value, 0);
+    var y = parseInt(price.value, 0);
     document.getElementById("total").textContent = (x * y).toString();
     document.getElementById("count").textContent = x.toString();
 }
--- food/main.ts
+++ food/main.ts
@@ -5,9 +5,23 @@ function beginRandomInt() {
     (document.getElementById("inputCount") as HTMLInputElement).value = generateRandomInteger(0, 50).toString();
     (document.getElementById("inputPrice") as HTMLInputElement).value = generateRandomInteger(50, 200).toString();
 }
+let count = (document.querySelector("#inputCount") as HTMLInputElement);
+let price = (document.querySelector("#inputPrice") as HTMLInputElement);
+let btn = (document.querySelector("#btn") as HTMLInputElement);
+count.addEventListener("blur", checkNull, false);
+price.addEventListener("blur", checkNull, false);
+btn.addEventListener("click", calc, false);
+function checkNull() {
+    if (this.value === "") {
+        alert("此欄位不得為空");
+        btn.setAttribute("disabled", "true");
+    } else {
+        btn.removeAttribute("disabled");
+    }
+}
 function calc() {
-    const x: number = parseInt((document.getElementById("inputCount") as HTMLInputElement).value, 0);
-    const y: number = parseInt((document.getElementById("inputPrice") as HTMLInputElement).value, 0);
+    const x: number = parseInt(count.value, 0);
+    const y: number = parseInt(price.value, 0);
     document.getElementById("total").textContent = (x * y).toString();
     document.getElementById("count").textContent = x.toString();
 }

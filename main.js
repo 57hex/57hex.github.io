d697f0f4f12b01e6c3b9e833f3819cde4ad4559e
WS <aa1010111@icloud.com>
2018-02-07 16:32:20 +0800

    add NightMode to the main page.

--- main.js
+++ main.js
@@ -16,12 +16,17 @@ var allWorks = [
         link: "food/index.html",
     },
 ];
+// 以上用來存作品相關資料。
+//
+var body = document.querySelector(".body");
 var list = document.querySelector(".list");
 var workName = document.querySelector(".workName");
 var workPath = document.querySelector(".workPath");
 var workHTML = document.querySelector(".workHtml");
 var workButton = document.querySelector(".workButton");
-workButton.addEventListener("click", addNewWork, false);
+// 這邊宣告從 ＨＴＭＬ 撈過來的變數。
+workButton.addEventListener("click", addNewWork, false); // 按按鈕時觸發 addNewWork 事件。
+//
 function listAll() {
     var strName = "";
     for (var i = 0; i < allWorks.length; i++) {
@@ -29,17 +34,27 @@ function listAll() {
     }
     list.innerHTML = strName;
 }
-listAll();
+// 更新菜單。
+listAll(); // 網頁被載入完的時候更新一次菜單。
+function closeResume() {
+    var resumeBeClosed = document.querySelector(".word");
+    if (resumeBeClosed.dataset.num === "0") {
+        resumeBeClosed.setAttribute("href", "#");
+    }
+}
+//
 function addToLocal() {
     localStorage.setItem("workName", workName.value);
     localStorage.setItem("workPath", workPath.value);
 }
+// 使用 Local Storage 儲存使用者在 InputBox 輸入的資料。
+//
 function addNewWork() {
     addToLocal();
     if (workName.value !== "" && workPath.value !== "") {
         var str = localStorage.getItem("workPath");
         if (workHTML.value !== "") {
-            str += "/" + workHTML.value + ".html";
+            str += "/" + workHTML.value + ".html"; // 自訂路徑補全 HTML
         }
         else {
             str += "/index.html";
@@ -52,3 +67,39 @@ function addNewWork() {
         return;
     }
 }
+// 儲存資料，檢查 InputBox 是否為空。
+// 夜間模式
+var seeMode = document.querySelector(".seeMode");
+var btn = document.querySelectorAll(".btn");
+var card = document.querySelector(".card-panel");
+var modeToken = 0;
+var bodyBGColor = body.style.backgroundColor;
+var btnBGColor = btn[0].style.backgroundColor;
+var cardBGColor = card.style.backgroundColor;
+var cardTextColor = card.style.color;
+localStorage.setItem("modeToken", JSON.stringify(modeToken));
+function changeMode() {
+    if (JSON.parse(localStorage.getItem("modeToken")) === 0) {
+        body.style.backgroundColor = "#262322";
+        for (var i = 0; i < btn.length; i++) {
+            btn[i].style.backgroundColor = "#485665";
+        }
+        card.style.backgroundColor = "#485665";
+        card.style.color = "#ffffff";
+        modeToken++;
+        seeMode.textContent = "夜間模式";
+        localStorage.setItem("modeToken", JSON.stringify(modeToken));
+    }
+    else if (JSON.parse(localStorage.getItem("modeToken")) !== 0) {
+        body.style.backgroundColor = bodyBGColor;
+        for (var i = 0; i < btn.length; i++) {
+            btn[i].style.backgroundColor = btnBGColor;
+        }
+        card.style.backgroundColor = cardBGColor;
+        card.style.color = cardTextColor;
+        seeMode.textContent = "日間模式";
+        modeToken--;
+        localStorage.setItem("modeToken", JSON.stringify(modeToken));
+    }
+}
+seeMode.addEventListener("click", changeMode, false);
--- main.ts
+++ main.ts
@@ -16,12 +16,17 @@ let allWorks = [
         link: "food/index.html",
     },
 ];
+// 以上用來存作品相關資料。
+//
+let body = document.querySelector(".body") as HTMLElement;
 let list = document.querySelector(".list");
 let workName = document.querySelector(".workName") as HTMLInputElement;
 let workPath = document.querySelector(".workPath") as HTMLInputElement;
 let workHTML = document.querySelector(".workHtml") as HTMLInputElement;
 let workButton = document.querySelector(".workButton");
-workButton.addEventListener("click", addNewWork, false);
+// 這邊宣告從 ＨＴＭＬ 撈過來的變數。
+workButton.addEventListener("click", addNewWork, false); // 按按鈕時觸發 addNewWork 事件。
+//
 function listAll() {
     let strName = "";
     for (let i = 0; i < allWorks.length; i++) {
@@ -29,17 +34,27 @@ function listAll() {
     }
     list.innerHTML = strName;
 }
-listAll();
+// 更新菜單。
+listAll(); // 網頁被載入完的時候更新一次菜單。
+function closeResume() {
+    const resumeBeClosed = document.querySelector(".word");
+    if (resumeBeClosed.dataset.num === "0") {
+        resumeBeClosed.setAttribute("href", "#");
+    }
+}
+//
 function addToLocal() {
     localStorage.setItem("workName", workName.value);
     localStorage.setItem("workPath", workPath.value);
 }
+// 使用 Local Storage 儲存使用者在 InputBox 輸入的資料。
+//
 function addNewWork() {
     addToLocal();
     if (workName.value !== "" && workPath.value !== "") {
         let str = localStorage.getItem("workPath");
         if (workHTML.value !== "") {
-            str += "/" + workHTML.value + ".html";
+            str += "/" + workHTML.value + ".html"; // 自訂路徑補全 HTML
         } else {
             str += "/index.html";
         }
@@ -50,3 +65,39 @@ function addNewWork() {
         return;
     }
 }
+// 儲存資料，檢查 InputBox 是否為空。
+
+// 夜間模式
+let seeMode = document.querySelector(".seeMode");
+let btn = document.querySelectorAll(".btn");
+let card = document.querySelector(".card-panel") as HTMLElement;
+let modeToken = 0;
+const bodyBGColor = body.style.backgroundColor;
+const btnBGColor = btn[0].style.backgroundColor;
+const cardBGColor = card.style.backgroundColor;
+const cardTextColor = card.style.color;
+localStorage.setItem("modeToken", JSON.stringify(modeToken));
+function changeMode() { // 改變夜間/日間模式
+    if (JSON.parse(localStorage.getItem("modeToken")) === 0){
+        body.style.backgroundColor = "#262322";
+        for (let i = 0; i < btn.length; i++) {
+            btn[i].style.backgroundColor = "#485665";
+        }
+        card.style.backgroundColor = "#485665";
+        card.style.color = "#ffffff";
+        modeToken ++;
+        seeMode.textContent = "夜間模式";
+        localStorage.setItem("modeToken", JSON.stringify(modeToken));
+    } else if (JSON.parse(localStorage.getItem("modeToken")) !== 0) {
+        body.style.backgroundColor = bodyBGColor;
+        for (let i = 0; i < btn.length; i++) {
+            btn[i].style.backgroundColor = btnBGColor;
+        }
+        card.style.backgroundColor = cardBGColor;
+        card.style.color = cardTextColor;
+        seeMode.textContent = "日間模式";
+        modeToken --;
+        localStorage.setItem("modeToken", JSON.stringify(modeToken));
+    }
+}
+seeMode.addEventListener("click", changeMode, false);

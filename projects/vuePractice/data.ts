4923602f89217712edd4ae0fc2607a08aa3668fd
WS <aa1010111@icloud.com>
2018-03-14 00:12:51 +0800

    測試性ㄉ實裝了登入註冊系統

--- projects/vuePractice/data.ts
+++ projects/vuePractice/data.ts
@@ -11,13 +11,13 @@ let config = {
 firebase.initializeApp(config)
 let filters = {
   all: function (todo: any) {
-	  let filter = []
-	  for (let obj of todo) {
-    if (obj.vis !== false) {
-      filter.push(obj)
+    let filter = []
+    for (let obj of todo) {
+      if (obj.vis !== false) {
+        filter.push(obj)
+      }
     }
-  }
-	  return filter
+    return filter
   },
   yetFinished: function (todo: any) {
     let filter = []
@@ -49,6 +49,8 @@ let app = new Vue({
     loginVis: 'login',
     account: '',
     password: '',
+	  loginErr: '',
+	  uid: '',
     regAccount: '',
     regPassword: '',
     regFirstName: '',
@@ -62,6 +64,12 @@ let app = new Vue({
     },
     filterNotFinishedWorks: function () {
       return filters.yetFinished(this.inputWorks)
+    },
+    loginPage: function () {
+      return (this.loginVis === 'login') && (this.login === false)
+    },
+    regPage: function () {
+      return (this.loginVis === 'register') && (this.login === false)
     }
   },
   methods: {
@@ -73,22 +81,22 @@ let app = new Vue({
         this.inputWorks.push({ content: todo, finished: false })
         localStorage.setItem('savedData', '')
         localStorage.setItem('savedData', JSON.stringify(this.inputWorks))
-	      firebase.database().ref(this.account).set(this.inputWorks)
+        firebase.database().ref(`${this.uid}/data`).set(this.inputWorks)
       }
     },
     deleteWork: function (todo) {
       this.inputWorks.splice(this.inputWorks.indexOf(todo), 1)
       localStorage.setItem(STORAGE_KEY, '')
       localStorage.setItem(STORAGE_KEY, JSON.stringify(this.inputWorks))
-      firebase.database().ref(this.account).set(this.inputWorks)
+	    firebase.database().ref(`${this.uid}/data`).set(this.inputWorks)
     },
     finishAllWorks: function () {
       for (let obj of this.inputWorks) {
         obj.finished = true
-	      localStorage.setItem(STORAGE_KEY, '')
-	      localStorage.setItem(STORAGE_KEY, JSON.stringify(this.inputWorks))
+        localStorage.setItem(STORAGE_KEY, '')
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(this.inputWorks))
       }
-	    firebase.database().ref(this.account).set(this.inputWorks)
+	    firebase.database().ref(`${this.uid}/data`).set(this.inputWorks)
     },
     finishWork: function (todo) {
       if (todo.finished === true) {
@@ -96,9 +104,9 @@ let app = new Vue({
       } else if (todo.finished === false) {
         todo.finished = true
       }
-	    localStorage.setItem(STORAGE_KEY, '')
-	    localStorage.setItem(STORAGE_KEY, JSON.stringify(this.inputWorks))
-	    firebase.database().ref(this.account).set(this.inputWorks)
+      localStorage.setItem(STORAGE_KEY, '')
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(this.inputWorks))
+	    firebase.database().ref(`${this.uid}/data`).set(this.inputWorks)
     },
     changeVisToAll: function () {
       return this.visibility = 'all'
@@ -116,42 +124,101 @@ let app = new Vue({
       return this.loginVis = 'login'
     },
     firebaseLogin: function () {
-      if (this.account !== '' || this.password !== '') {
-        const fire = firebase.database().ref(app.account)
-        fire.once('value', function (s) {
-          if (s.val() === null || s.val().length === 0 || app.inputWorks === null) {
-            fire.set([{  vis: false }])
-            app.loading = false
-            app.inputWorks = [{ vis: false }]
-          }
-          localStorage.setItem(STORAGE_KEY, JSON.stringify(s.val()))
-          app.loading = false
-          app.inputWorks = (JSON.parse(localStorage.getItem(STORAGE_KEY)))
-		        }
-        )
-	      this.login = true
-	      // return firebase.database().ref(this.account).set(this.inputWorks)
-      } else {
-        alert('你好像忘記打帳號ㄌ')
-      }
-    },
+	    if (app.account !== '') {
+		    firebase.auth().signInWithEmailAndPassword(app.account, app.password).then(
+				    function (user) {
+					    const uid = user.uid
+					    app.uid = uid
+					    app.login = true
+					    const fire = firebase.database().ref(`${app.uid}/data`)
+					    fire.once('value', function (s) {
+								    if (s.val() === null || s.val().length === 0 || app.inputWorks === null) {
+									    fire.set([{ vis: false }])
+									    app.loading = false
+									    app.inputWorks = [{ vis: false }]
+								    }
+								    localStorage.setItem(STORAGE_KEY, JSON.stringify(s.val()))
+								    app.loading = false
+								    app.inputWorks = (JSON.parse(localStorage.getItem(STORAGE_KEY)))
+							    }
+					    ).catch(
+							    function (error) {
+								    let errorCode = error.code
+								    let errorMsg = error.message
+								    app.loginErr = errorMsg
+								    if (app.loginErr === 'auth/wrong-password') {
+									    app.loginErr = '密碼錯誤。'
+								    } else if (app.loginErr === 'auth/invalid-email') {
+									    app.loginErr = '信箱格式錯誤。'
+								    }
+							    }
+					    )
+				    })
+	    } else if (app.password !== '') {
+		    firebase.auth().signInWithEmailAndPassword(app.account, app.password).then(
+				    function (user) {
+					    app.uid = user.uid
+					    const fire = firebase.database().ref(`${this.uid}/data`)
+					    fire.once('value', function (s) {
+								    if (s.val() === null || s.val().length === 0 || app.inputWorks === null) {
+									    fire.set([{ vis: false }])
+									    app.loading = false
+									    app.inputWorks = [{ vis: false }]
+								    }
+								    localStorage.setItem(STORAGE_KEY, JSON.stringify(s.val()))
+								    app.loading = false
+								    app.inputWorks = (JSON.parse(localStorage.getItem(STORAGE_KEY)))
+							    }
+					    ).catch(
+							    function (error) {
+								    let errorCode = error.code
+								    let errorMsg = error.message
+								    app.loginErr = errorMsg
+								    if (app.loginErr === 'auth/wrong-password') {
+									    app.loginErr = '密碼錯誤。'
+								    } else if (app.loginErr === 'auth/invalid-email') {
+									    app.loginErr = '信箱格式錯誤。'
+								    } else if (app.loginErr === '') {
+									    app.loginErr = ''
+								    }
+							    }
+					    )
+				    })
+	    } else {
+		    alert('你忘記打帳號密碼了！')
+	    }
+	    },
     firebaseReg: function () {
-      firebase.auth().createUserWithEmailAndPassword(app.regAccount, app.regPassword).catch(
-          function (error) {
-            let errorCode = error.code
-            let errorMsg = error.message
-            app.regError = errorMsg
-            if (app.regError === 'The email address is badly formatted.') {
-              app.regError = '帳號或是密碼格式錯誤。'
-            } else if (app.regError === 'Password should be at least 6 characters') {
-              app.regError = '密碼必須大於六個字。'
-            } else if (app.regError === 'The email address is already in use by another account.') {
-              app.regError = '這個帳號已被註冊。'
-            } else if (app.regError === 'The password must be 6 characters long or more.') {
-              app.regError = '密碼必須大於六個字母。'
-            }
-          }
-    }
-    )
+      firebase.auth().createUserWithEmailAndPassword(app.regAccount, app.regPassword).then(function (user) {
+	      app.uid = user.uid
+	      const fire = firebase.database().ref(`${app.uid}/data`)
+	      fire.set([{ vis: false }])
+	      app.login = true
+	      fire.once('value', function (s) {
+		      if (s.val() === null || s.val().length === 0 || app.inputWorks === null) {
+			      fire.set([{ vis: false }])
+			      app.loading = false
+			      app.inputWorks = [{ vis: false }]
+		      }
+		      localStorage.setItem(STORAGE_KEY, JSON.stringify(s.val()))
+		      app.loading = false
+		      app.inputWorks = (JSON.parse(localStorage.getItem(STORAGE_KEY)))
+	      }
+      }).catch(
+					function (error) {
+  let errorCode = error.code
+  let errorMsg = error.message
+  console.log(error.code)
+  app.regError = errorCode
+  if (app.regError !== '') {
+	  if (app.regError === 'auth/invalid-email') {
+		  app.regError = '信箱格式錯誤。'
+	  } else if (app.regError === 'auth/weak-password') {
+		  app.regError = '密碼必須大於六個字。'
+	  } else if (app.regError === 'auth/email-already-in-use') {
+		  app.regError = '這個帳號已被註冊。'
+	  }
+  }
+})
     }
   })

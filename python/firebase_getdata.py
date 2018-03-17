145ba21891798503e67691277601ab625382ba4c
ZilongXu <aa1010111@icloud.com>
2018-03-17 17:13:12 +0800

    更新firebase_getdata

--- python/firebase_getdata.py
+++ python/firebase_getdata.py
@@ -1,10 +1,11 @@
 from firebase import firebase
+import json
 
-GET_YOUR_OWN_TOKEN = "Go get your own god-damn token"
-i = 1
+GET_YOUR_OWN_TOKEN = "ZNIJipjwcaZ1huybwWKlc6NzHFO5CdQaj1B0c31i"
 email = input(str("Enter your own email(starts with /):"))
 email1 = email.split(".")
 email = str("_".join(email1) + "/data")
+print(email)
 
 authentication = firebase.FirebaseAuthentication(GET_YOUR_OWN_TOKEN, 'Your own email', True, True)
 firebase.authentication = authentication
@@ -13,31 +14,29 @@ user = authentication.get_user()
 firebase = firebase.FirebaseApplication('https://project-4fe4c.firebaseio.com', authentication=authentication)
 
 
-def showUnfinish():
-    while True:
-        count = str(i)
-        result = firebase.get(email, count)
-        if result['finished'] == "False"
-            i += 1
-            continue
-        if not result:
-            break
-        print(result['content'])
-        i += 1
-        
-        
 def showFinish():
+    i = 0
     while True:
         count = str(i)
         result = firebase.get(email, count)
-        if result['finished'] == "True"
-            i += 1
-            continue
         if not result:
             break
-        print(result['content'])
+        if result['finished'] == True:
+            print(result['content'])
         i += 1
-        
-        
+
+
+def showUnfinish():
+    i = 0
+    while True:
+        count = str(i)
+        result = firebase.get(email, count)
+        if not result:
+            break
+        if result['finished'] == False:
+            print(result['content'])
+        i += 1
+
+
 showFinish()
 showUnfinish()

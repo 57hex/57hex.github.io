f9fc43fcc2af04943f8bfb0f046d4b8efac44ccd
화선칸 <67inhex@gmail.com>
2018-08-26 16:51:04 +0800

    safari zoom fixed

--- projects/enl/js/main.js
+++ projects/enl/js/main.js
@@ -6,7 +6,20 @@ window.onload = function () {
     } else {
         width =  self.innerWidth;
     }
-}
+    document.addEventListener('touchstart', function (event) {
+      if (event.touches.length > 1) {
+        event.preventDefault();
+        }
+      }, { passive: false });
+      let lastTouchEnd = 0;
+      document.addEventListener('touchend', function (event) {
+          const now = (new Date()).getTime();
+          if ( now - lastTouchEnd <= 300) {
+            event.preventDefault();
+          }
+          lastTouchEnd = now;
+      }, false)
+    }
 window.addEventListener('resize', function () {
   if (isIE) {
     width =  document.compatMode === "CSS1Compat" ? document.documentElement.clientWidth : document.body.clientWidth;

6a556575babd013d3f9185098090f7e982d785fd
화선칸 <67inhex@gmail.com>
2018-08-30 14:43:56 +0800

    ＲＷＤ修正

--- enlTest/index.js
+++ enlTest/index.js
@@ -5,15 +5,19 @@ let countDownDay = document.getElementById("count-down-day");
 let bg_tra1 = document.getElementById("bg-tra1");
 let bg_tra2 = document.getElementById("bg-tra2");
 let bg_tra3 = document.getElementById("bg-tra3");
+let introContent = document.getElementById("intro-inner-content")
 let isIE = /*@cc_on!@*/false || !!document.documentMode;
 let height = 0;
+let width = 0;
 window.onload = () => {
   betweenNowAndEndDay = Math.floor((end.valueOf() - now.valueOf()) / 86400000);
   countDownDay.innerHTML = `還剩 <b>${betweenNowAndEndDay}</b> 天`;
   if (isIE) {
     height =  document.compatMode === "CSS1Compat" ? document.documentElement.clientHeight : document.body.clientHeight;
+    width =  document.compatMode === "CSS1Compat" ? document.documentElement.clientWidth : document.body.clientWidth;
   } else {
     height =  self.innerHeight;
+    width = self.innerWidth;
   }
   document.addEventListener('touchstart', function (event) {
     if (event.touches.length > 1) {
@@ -35,10 +39,17 @@ window.onload = () => {
 window.addEventListener('resize', function () {
   if (isIE) {
     height =  document.compatMode === "CSS1Compat" ? document.documentElement.clientHeight : document.body.clientHeight;
+    width =  document.compatMode === "CSS1Compat" ? document.documentElement.clientWidth : document.body.clientWidth;
   } else {
     height =  self.innerHeight;
+    width =  self.innerWidth;
   }
   bg_tra1.style.borderWidth = `0 0 ${height * 0.2}px ${height * 0.2}px`
   bg_tra2.style.borderWidth = `0 0 ${height * 0.6}px ${height * 0.6}px`
   bg_tra3.style.borderWidth = `0 0 ${height}px ${height}px`
+  if (width < 346) {
+    introContent.innerHTML = "2018 <span>/</span> <br> 10 <span>/</span> 20<br><b>台中</b><br><span class='intro-phone-text-color'>Recursion<br>Prime</span>"
+  } else {
+    introContent.innerHTML = "2018 <span>/</span> 10 <span>/</span> 20<br> <div class='intro-TC-RP-inline'> <b>台中</b><span>Recursion<br>Prime</span></div>"
+  }
 })

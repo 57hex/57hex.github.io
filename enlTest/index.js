672a184b1f3f347d5f757a7336f254bcd76bcd5f
화선칸 <67inhex@gmail.com>
2018-08-31 17:02:06 +0800

    現在時鐘會倒數了

--- enlTest/index.js
+++ enlTest/index.js
@@ -1,50 +1,58 @@
-"use strict";
+let test = document.querySelector('.test');
+let now = new Date();
+let end = new Date("2018-10-20T00:00:00");
+let countDownDay = document.getElementById("count-down-day");
+let bg_tra1 = document.getElementById("bg-tra1");
+let bg_tra2 = document.getElementById("bg-tra2");
+let bg_tra3 = document.getElementById("bg-tra3");
+let introContent = document.getElementById("intro-inner-content")
+let isIE = /*@cc_on!@*/false || !!document.documentMode;
+let height = 0;
+let width = 0;
+function startCountDown() {
+    now = new Date();
+    end = new Date("2018-10-20T00:00:00");
+    let betweenMS = (end - now);
+    let day = betweenMS / 1000 / 60 / 60 / 24;
+    let hour = (day - Math.floor(day)) * 24;
+    let min = (hour - Math.floor(hour)) * 60;
+    let secOnd = (min - Math.floor(min)) * 60;
+    countDownDay.innerHTML = `還剩<br><b>${Math.floor(day)} <span>天</span> ${Math.floor(hour)} <span>小時</span> ${Math.floor(min)} <span>分</span> ${Math.floor(secOnd)} <span>秒</span><b>`
+}
 
-var now = new Date();
-var end = new Date("2018-10-20");
-var betweenNowAndEndDay = 0;
-var countDownDay = document.getElementById("count-down-day");
-var bg_tra1 = document.getElementById("bg-tra1");
-var bg_tra2 = document.getElementById("bg-tra2");
-var bg_tra3 = document.getElementById("bg-tra3");
-var introContent = document.getElementById("intro-inner-content");
-var isIE = /*@cc_on!@*/false || !!document.documentMode;
-var height = 0;
-var width = 0;
-window.onload = function () {
-  betweenNowAndEndDay = Math.ceil((end - now) / 86400000);
-  if (betweenNowAndEndDay > 0) {
-    countDownDay.innerHTML = "\u9084\u5269 <b>" + betweenNowAndEndDay + "</b> \u5929";
-  }
-  if (isIE) {
-    height = document.compatMode === "CSS1Compat" ? document.documentElement.clientHeight : document.body.clientHeight;
-    width = document.compatMode === "CSS1Compat" ? document.documentElement.clientWidth : document.body.clientWidth;
-  } else {
-    height = self.innerHeight;
-    width = self.innerWidth;
-  }
-  document.addEventListener('touchstart', function (event) {
-    if (event.touches.length > 1) {
-      event.preventDefault();
+window.onload = () => {
+    let betweenNowAndEndDay = Math.ceil((end-now) / 86400000);
+    if (betweenNowAndEndDay > 0) {
+        setInterval(startCountDown, 1000);
     }
-  }, { passive: false });
-  var lastTouchEnd = 0;
-  document.addEventListener('touchend', function (event) {
-    var now = new Date().getTime();
-    if (now - lastTouchEnd <= 300) {
-      event.preventDefault();
-      lastTouchEnd = now;
+    if (isIE) {
+        height =  document.compatMode === "CSS1Compat" ? document.documentElement.clientHeight : document.body.clientHeight;
+        width =  document.compatMode === "CSS1Compat" ? document.documentElement.clientWidth : document.body.clientWidth;
+    } else {
+        height =  self.innerHeight;
+        width = self.innerWidth;
     }
-  }, false);
-  bg_tra1.style.borderWidth = "0 0 " + height * 0.2 + "px " + height * 0.2 + "px";
-  bg_tra2.style.borderWidth = "0 0 " + height * 0.6 + "px " + height * 0.6 + "px";
-  bg_tra3.style.borderWidth = "0 0 " + height + "px " + height + "px";
-  if (width < 346) {
-    introContent.innerHTML = "2018 <span>/</span> <br> 10 <span>/</span> 20<br><b>台中</b><br><span class='intro-phone-text-color'>Recursion<br>Prime</span>";
-  } else {
-    introContent.innerHTML = "2018 <span>/</span> 10 <span>/</span> 20<br> <div class='intro-TC-RP-inline'> <b>台中</b><span>Recursion<br>Prime</span></div>";
-  }
-};
+    document.addEventListener('touchstart', function (event) {
+        if (event.touches.length > 1) {
+            event.preventDefault();
+        }
+    }, { passive: false });
+    let lastTouchEnd = 0;
+    document.addEventListener('touchend', function (event) {
+        const now = (new Date()).getTime();
+        if ( now - lastTouchEnd <= 300) {
+            event.preventDefault();
+            lastTouchEnd = now;
+        }}, false)
+    bg_tra1.style.borderWidth = `0 0 ${height * 0.2}px ${height * 0.2}px`
+    bg_tra2.style.borderWidth = `0 0 ${height * 0.6}px ${height * 0.6}px`
+    bg_tra3.style.borderWidth = `0 0 ${height}px ${height}px`
+    if (width < 346) {
+        introContent.innerHTML = "2018 <span>/</span> <br> 10 <span>/</span> 20<br><b>台中</b><br><span class='intro-phone-text'>Recursion<br>Prime</span>"
+    } else {
+        introContent.innerHTML = "2018 <span>/</span> 10 <span>/</span> 20<br> <div class='intro-TC-RP-inline'> <b>台中</b><span class='text-yellow'>Recursion<br>Prime</span></div>"
+    }
+}
 window.addEventListener('resize', function () {
     if (isIE) {
         height = document.compatMode === "CSS1Compat" ? document.documentElement.clientHeight : document.body.clientHeight;
@@ -53,13 +61,12 @@ window.addEventListener('resize', function () {
         height = self.innerHeight;
         width = self.innerWidth;
     }
-    bg_tra1.style.borderWidth = "0 0 " + height * 0.2 + "px " + height * 0.2 + "px";
-    bg_tra2.style.borderWidth = "0 0 " + height * 0.6 + "px " + height * 0.6 + "px";
-    bg_tra3.style.borderWidth = "0 0 " + height + "px " + height + "px";
+    bg_tra1.style.borderWidth = `0 0 ${height * 0.2}px ${height * 0.2}px`
+    bg_tra2.style.borderWidth = `0 0 ${height * 0.6}px ${height * 0.6}px`
+    bg_tra3.style.borderWidth = `0 0 ${height}px ${height}px`
     if (width < 346) {
-        introContent.innerHTML = "2018 <span>/</span> <br> 10 <span>/</span> 20<br><b>台中</b><br><span class='intro-phone-text-color'>Recursion<br>Prime</span>";
+        introContent.innerHTML = "2018 <span>/</span> <br> 10 <span>/</span> 20<br><b>台中</b><br><span class='intro-phone-text-color'>Recursion<br>Prime</span>"
     } else {
-        introContent.innerHTML = "2018 <span>/</span> 10 <span>/</span> 20<br> <div class='intro-TC-RP-inline'> <b>台中</b><span>Recursion<br>Prime</span></div>";
+        introContent.innerHTML = "2018 <span>/</span> 10 <span>/</span> 20<br> <div class='intro-TC-RP-inline'> <b>台中</b><span>Recursion<br>Prime</span></div>"
     }
-});
-//# sourceMappingURL=index.js.map
+})
